d1b2046f1a034ea7138b90626853d0125da68108
mouha080 <walkerkyleissue@gmail.com>
2026-03-15 08:59:36 +0000

    last mise à jour

--- src/data/products.js
+++ src/data/products.js
@@ -1,7 +1,6 @@
 import iphone17 from "../assets/iphone17.jpg"
 import headphone from "../assets/headphone.jpg"
 import samsung from "../assets/samsung.jpg"
-import redmi from "../assets/redmi.jpg"
 import airpods from "../assets/airpod.jpg"
 import casque from "../assets/casque.jpg"
 import coque from "../assets/coque.jpg"
@@ -12,16 +11,52 @@ import airpod2 from "../assets/airpod2.jpg"
 import iphonexr from "../assets/iphonexr.jpg"
 import iphone13 from "../assets/iphone13.jpg"
 import simple from "../assets/iphone16simple.jpg"
-import huawei from "../assets/Huaweinova.jpg"
 import onze from "../assets/11simple.jpeg"
 import iphone11proo from "../assets/11pro.jpg"
 import iphone11pro from "../assets/iphone11progold.jpeg"
 import iphone11promax from "../assets/11promax.jpg"
 import iphone17pro from "../assets/17proo.jpg"
 import powerbank from "../assets/pb.jpg"
-import iphone15promax from "../assets/16promax.png"
+import iphone16promax from "../assets/16promax.png"
 import iphone15plus from "../assets/iphone15plus.png"
 import ecouteur from "../assets/Ecouteur.jpg"
+import iphone14pro from "../assets/14pro.jpg"
+import iphone14pm from "../assets/14promax.jpg"
+import iphone17air from "../assets/17air.jpg"
+import iphone17airr from "../assets/17airr.jpg"
+import iphone17proesim from "../assets/17proesim.jpg"
+import iphone16promaxx from "../assets/16prooo.jpg"
+import iphone12pro from "../assets/12pro.jpg"
+import iphone15promax from "../assets/15promax.jpeg"
+import iphone16plus from "../assets/16plus.jpg"
+import iphone16pluss from "../assets/16pluss.png"
+import iphonexrr from "../assets/xrrr.jpg"
+import samsungs22u from "../assets/s22ul.png"
+import samsungs23 from "../assets/galaxys23.jpg"
+import samsungs22sc from "../assets/s22ultra.jpeg"
+import samsungs23sc from "../assets/s23ultra.jpg"
+import samsungs22p from "../assets/s22plus.jpg"
+import huaweip30 from "../assets/huaweip30.jpg"
+import taba8 from "../assets/taba8.jpg"
+import taba9plus from "../assets/taba9plus.jpeg"
+import taba9p from "../assets/tab9plus.jpg"
+import taba9s from "../assets/a9simple.jpg"
+import watchs5 from "../assets/watchs5.jpg"
+import watchs6 from "../assets/watchs6.jpg"
+import watchs7 from "../assets/watchs7.jpg"
+import watchs8 from "../assets/watchs8.jpg"
+import watchs9 from "../assets/watchs9.jpeg"
+import air1 from "../assets/ipadair1.jpg"
+import air2 from "../assets/ipadair2.jpg"
+import ipad6th from "../assets/ipad6th.png"
+import ipad7th from "../assets/ipad7th.jpg"
+import ipad11th from "../assets/ipad11th.png"
+import ipadpro9 from "../assets/ipadpro9.jpg"
+import ipadairm3 from "../assets/ipadairm3inch.jpg"
+import ipadpro10 from "../assets/ipadpro10.jpg"
+import ipadpro11inch from "../assets/ipadpro11inch4th.png"
+import ipadprom4 from "../assets/ipadproinchm4.jpg"
+
 
 
 export const products = [
@@ -69,24 +104,6 @@ price:"20 000 FCFA",
 image:headphone,
 category:"accessory",
 description:"Casque JBL avec un son puissant, des basses profondes et un grand confort pour écouter de la musique pendant de longues périodes."
-},
-
-{
-id:3,
-name:"Samsung Galaxy S21",
-price:"400 000 FCFA",
-image:samsung,
-category:"phone",
-description:"Le Samsung Galaxy S21 offre un design moderne, un écran AMOLED de haute qualité et un appareil photo performant pour des photos nettes."
-},
-
-{
-id:4,
-name:"Redmi Note 12",
-price:"180 000 FCFA",
-image:redmi,
-category:"phone",
-description:"Le Redmi Note 12 est un smartphone performant avec une bonne autonomie, un écran fluide et des performances adaptées aux usages quotidiens."
 },
 
 {
@@ -143,14 +160,6 @@ category:"phone",
 description:"Etat battérie: 100%, Stockage: 128GO"
 },
 
-{
-id:14,
-name:"Huawei Nova",
-price:"150 000 FCFA",
-image:huawei,
-category:"phone",
-description:"Huawei Nova est un smartphone élégant offrant un bon équilibre entre performance, autonomie et qualité photo."
-},
 
 {
 id:15,
@@ -182,19 +191,28 @@ description:"Etat battérie: 90% et plus, Stockage: 256GO"
 {
 id:19,
 name:"iPhone 11 Pro Max",
-price:"150 000 FCFA",
+price:"160 000 FCFA",
 image:iphone11promax,
 category:"phone",
-description:"Etat battérie: 90% et plus, Stockage: 64GO"
+description:"Etat battérie: 90% et plus, Stockage: 256GO"
+},
+
+{
+id:16,
+name:"iPhone 11 simple",
+price:"125 000 FCFA",
+image:onze,
+category:"phone",
+description:"Etat battérie: 90% et plus, Stockage: 64GO."
 },
 
 {
 id:20,
-name:"iPhone 15 Plus",
-price:"350 000 FCFA",
+name:"iPhone 15 Plus Esim",
+price:"300 000 FCFA",
 image:iphone15plus,
 category:"phone",
-description:"Etat battérie: 90% et plus, Stockage: 64GO"
+description:"Etat battérie: 90% et plus, Stockage: 128GO"
 },
 
 {
@@ -209,10 +227,10 @@ description:"Etat battérie: 100%, Stockage: 256GO"
 {
 id:22,
 name:"iPhone 16 Pro Max",
-price:"600 000 FCFA",
-image:iphone15promax,
+price:"580 000 FCFA",
+image:iphone16promax,
 category:"phone",
-description:"Etat battérie: 90% et plus, Stockage: 128GO"
+description:"Etat battérie: 90% et plus, Stockage: 256GO"
 },
 
 {
@@ -232,16 +250,338 @@ image:ecouteur,
 category:"accessory",
 },
 
+{
+id:25,
+name:"Iphone XR",
+price:"120 000 FCFA",
+image:iphonexrr,
+category:"phone",
+description:"Etat battérie: 90% et plus, Stockage: 256GO"
+},
+
+{
+id:26,
+name:"iPhone 14 Pro Esim",
+price:"250 000 FCFA",
+image:iphone14pro,
+category:"phone",
+description:"Etat battérie: 90% et plus, Stockage: 128GO"
+},
+
+{
+id:35,
+name:"iPhone 14 Pro Max Esim",
+price:"300 000 FCFA",
+image:iphone14pm,
+category:"phone",
+description:"Etat battérie: 90% et plus, Stockage: 128GO"
+},
+
+{
+id:27,
+name:"iPhone 16 Pro Max",
+price:"600 000 FCFA",
+image:iphone16promaxx,
+category:"phone",
+description:"Etat battérie: 100%, Stockage: 512GO"
+},
+
+{
+id:28,
+name:"iPhone 16 Plus",
+price:"445 000 FCFA",
+image:iphone16plus,
+category:"phone",
+description:"Etat battérie: 90% et plus, Stockage: 128GO"
+},
+
+{
+id:29,
+name:"iPhone 16 Plus 256",
+price:"500 000 FCFA",
+image:iphone16pluss,
+category:"phone",
+description:"Etat battérie: 90% et plus, Stockage: 256GO"
+},
+
+{
+id:30,
+name:"iPhone 12 Pro",
+price:"170 000 FCFA",
+image:iphone12pro,
+category:"phone",
+description:"Etat battérie: 90% et plus, Stockage: 128GO"
+},
+
+{
+id:33,
+name:"iPhone 15 Pro Max",
+price:"430 000 FCFA",
+image:iphone15promax,
+category:"phone",
+description:"Etat battérie: 90% et plus, Stockage: 128GO"
+},
+
+{
+id:31,
+name:"iPhone 17 Air",
+price:"650 000 FCFA",
+image:iphone17air,
+category:"phone",
+description:"Etat battérie: 100%, Stockage: 256GO"
+},
+
+{
+id:32,
+name:"iPhone 17 Air 512",
+price:"680 000 FCFA",
+image:iphone17airr,
+category:"phone",
+description:"Etat battérie: 100%, Stockage: 512GO"
+},
+
+{
+id:34,
+name:"iPhone 17 Pro Esim",
+price:"780 000 FCFA",
+image:iphone17proesim,
+category:"phone",
+description:"Etat battérie: 100%, Stockage: 512GO"
+},
+
+{
+id:35,
+name:"Samsung S22 PLUS",
+price:"280 000 FCFA",
+image:samsungs22p,
+category:"phone",
+description:"Stockage: 128GO"
+},
+
+{
+id:36,
+name:"Samsung S22 ULTRA",
+price:"300 000 FCFA",
+image:samsungs22u,
+category:"phone",
+description:"Stockage: 128GO"
+},
+
+{
+id:37,
+name:"Samsung S23 ULTRA",
+price:"380 000 FCFA",
+image:samsungs23,
+category:"phone",
+description:"Stockage: 256GO"
+},
+
+{
+id:3,
+name:"Samsung S21 ULTRA",
+price:"215 000 FCFA",
+image:samsung,
+category:"phone",
+description:"Stockage: 128GO"
+},
+
+{
+id:38,
+name:"Samsung S22 ULTRA SCELLE",
+price:"330 000 FCFA",
+image:samsungs22sc,
+category:"phone",
+description:"Stockage: 128GO"
+},
+
+{
+id:14,
+name:"Huawei P30 Lite",
+price:"500 000 FCFA",
+image:huaweip30,
+category:"phone",
+description:"Stockage: 128GO"
+},
 
 
 {
-id:16,
-name:"iPhone 11 simple",
-price:"120 000 FCFA",
-image:onze,
+id:39,
+name:"Samsung S23 ULTRA SCELLE",
+price:"450 000 FCFA",
+image:samsungs23sc,
 category:"phone",
-description:"Etat battérie: 90% et plus, Stockage: 64GO."
+description:"Stockage: 256GO"
+},
+
+{
+id:40,
+name:"Tablette Samsung Tab A8",
+price:"75 000 FCFA",
+image:taba8,
+category:"phone",
+description:"Stockage: 32GO, Sans sim"
+},
+
+{
+id:41,
+name:"Tablette Samsung Tab A9 Simple",
+price:"90 000 FCFA",
+image:taba9s,
+category:"phone",
+description:"Stockage: 64GO"
+},
+
+{
+id:42,
+name:"Tablette Samsung Tab A9 Plus",
+price:"135 000 FCFA",
+image:taba9p,
+category:"phone",
+description:"Stockage: 64GO"
+},
+
+{
+id:43,
+name:"Tablette Samsung Tab A9 Plus",
+price:"170 000 FCFA",
+image:taba9plus,
+category:"phone",
+description:"Stockage: 128GO"
+},
+
+{
+id:44,
+name:"Watch Serie 5: 44mm",
+price:"95 000 FCFA",
+image:watchs5,
+category:"accessory",
+},
+
+{
+id:44,
+name:"Watch Serie 6: 44mm Saphir",
+price:"140 000 FCFA",
+image:watchs6,
+category:"accessory",
+},
+
+{
+id:44,
+name:"Watch Serie 7: 41mm",
+price:"115 000 FCFA",
+image:watchs7,
+category:"accessory",
+},
+
+{
+id:44,
+name:"Watch Serie 8: 45mm",
+price:"170 000 FCFA",
+image:watchs8,
+category:"accessory",
+},
+
+{
+id:44,
+name:"Watch Serie 9: 45mm",
+price:"200 000 FCFA",
+image:watchs9,
+category:"accessory",
+},
+
+{
+id:45,
+name:"Ipad Air 1",
+price:"45 000 FCFA",
+image:air1,
+category:"phone",
+description:"Stockage: 32GO"
+},
+
+{
+id:46,
+name:"Ipad Air 2",
+price:"65 000 FCFA",
+image:air2,
+category:"phone",
+description:"Stockage: 64GO"
+},
+
+{
+id:47,
+name:"Ipad 6th",
+price:"85 000 FCFA",
+image:ipad6th,
+category:"phone",
+description:"Stockage: 128GO"
+},
+
+{
+id:48,
+name:"Ipad 7th",
+price:"96 000 FCFA",
+image:ipad7th,
+category:"phone",
+description:"Stockage: 32GO"
+},
+
+{
+id:49,
+name:"Ipad Pro 9.7",
+price:"125 000 FCFA",
+image:ipadpro9,
+category:"phone",
+description:"Stockage: 32GO"
+},
+
+{
+id:50,
+name:"Ipad Pro 10.5",
+price:"150 000 FCFA",
+image:ipadpro10,
+category:"phone",
+description:"Stockage: 64GO"
+},
+
+{
+id:51,
+name:"Ipad 11th A16 SCELLE",
+price:"275 000 FCFA",
+image:ipad11th,
+category:"phone",
+description:"Stockage: 128GO"
+},
+
+{
+id:52,
+name:"Ipad Pro 11 Inch 4th Gen",
+price:"430 000 FCFA",
+image:ipadpro11inch,
+category:"phone",
+description:"Stockage: 128GO"
+},
+
+{
+id:53,
+name:"Ipad Air M3 11 Inch",
+price:"460 000 FCFA",
+image:ipadairm3,
+category:"phone",
+description:"Stockage: 128GO"
+},
+
+{
+id:54,
+name:"Ipad Pro M4 13inch ",
+price:"570 000 FCFA",
+image:ipadprom4,
+category:"phone",
+description:"Stockage: 256GO"
 }
+
+
+
+
 
 
 ]
